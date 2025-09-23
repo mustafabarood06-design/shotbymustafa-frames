3d19597fcdf5b09beeabd92377ebb3293c895336
mustafabarood06-design <mustafabarood06@gmail.com>
2025-09-23 19:39:12 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,8 @@
+// Example React component
+export default function FrontPage() {
+  return (
+    <div style={{ backgroundColor: "#f0f0f0", minHeight: "100vh" }}>
+      {/* Your content */}
+    </div>
+  );
+}
