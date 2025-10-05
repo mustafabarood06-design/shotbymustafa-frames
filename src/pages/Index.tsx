18ddcbade9320a2f6b1d63f46f5a819b4b15a644
mustafabarood06-design <mustafabarood06@gmail.com>
2025-10-05 16:22:21 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -88,7 +88,7 @@ const Index = () => {
   };
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-blue-300">
       <VisitorNotification />
       <AdminDashboard />
       <Navigation scrollToSection={scrollToSection} />
